ba1a71fd9bf04cdfecb3c79cca347965996357f4
pmuia <muthama54@gmail.com>
2025-05-21 16:34:38 +0300

    Add interface for entity in the domain project

---
+++ src/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.API
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddApiService(this IServiceCollection services)
+		{
+			return services;
+		}
+	}
+}
